e3d25cdb8c10a030d523d05597207df08877b603
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-05-20 05:12:53 +0330

    Attach crash routes to deployed app

--- src/crash-routes.ts
+++ src/crash-routes.ts
@@ -1,4 +1,4 @@
-import app from './index';
+import app from './index-with-tgs-overlay';
 import { ensureCrashVirtualColumns, revealCrashVirtualCashouts, seedCrashVirtualUsers } from './crash-virtual-users';
 
 const CACHE_NONE = 'no-store';
