80afda9332052c15ee258cc52cb510cdff6c7df5
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-05-31 18:59:30 +0330

    Revert "Mines: friend-round credit settlement, result state and exit control"

--- migrations/0008_mines_friend_credit_results.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE mines_friend_rooms ADD COLUMN amount_nano INTEGER NOT NULL DEFAULT 10000000;
-ALTER TABLE mines_friend_rooms ADD COLUMN winner_user_id TEXT;
-ALTER TABLE mines_friend_rooms ADD COLUMN credit_settled_at TEXT;
