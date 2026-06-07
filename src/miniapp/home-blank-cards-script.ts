90103b79bde7df92963d17d7c86bf64ddad5b7d0
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-07 10:15:59 +0330

    Remove stale blank card styles hiding daily rewards

--- src/miniapp/home-blank-cards-script.ts
+++ src/miniapp/home-blank-cards-script.ts
@@ -1,5 +1,6 @@
 export const HOME_BLANK_CARDS_SCRIPT = `
 (function(){
   document.querySelectorAll('#homeBlankCardsWrap').forEach(function(n){try{n.remove()}catch(e){}});
+  document.querySelectorAll('#homeBlankCardsStyle').forEach(function(n){try{n.remove()}catch(e){}});
 })();
 `;
