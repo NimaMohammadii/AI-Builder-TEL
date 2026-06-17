dcb5ed0e4836291a0549222b935791eaad98f387
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-17 21:06:09 +0330

    Update imports

--- src/vexa-voice-message-routes.ts
+++ src/vexa-voice-message-routes.ts
@@ -1,4 +1,5 @@
 import app from './index';
+import './vexa-voice-target-user-routes';
 import { registerVexaVoiceMessageRoutes } from './vexa-voice-message-api';
 
 registerVexaVoiceMessageRoutes(app as never);
