45b0f82f8cd48d72a8e687c5989062475ad88357
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-22 06:57:48 +0330

    Apply page fit style

--- src/miniapp/ghost-run/index.ts
+++ src/miniapp/ghost-run/index.ts
@@ -1,2 +1,5 @@
 export { GHOST_RUN_SECTION } from './section';
-export { GHOST_RUN_STYLES } from './styles';
+import { GHOST_RUN_STYLES as BASE_GHOST_RUN_STYLES } from './styles';
+import { GHOST_RUN_PAGE_FIT } from './page-fit';
+
+export const GHOST_RUN_STYLES = BASE_GHOST_RUN_STYLES + GHOST_RUN_PAGE_FIT;
