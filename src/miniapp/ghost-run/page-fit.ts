acefae5e8962fec53def11f0382ad6bc2790534d
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-22 07:00:12 +0330

    Remove unused module

--- src/miniapp/ghost-run/page-fit.ts
+++
@@ -1,6 +0,0 @@
-export const GHOST_RUN_PAGE_FIT = `
-#ghostrun .ghost-run-screen{width:calc(100% + 36px)!important;margin-left:-18px!important;margin-right:-18px!important;background:transparent!important}
-#ghostrun .ghost-run-scene{border-radius:0!important;box-shadow:none!important;width:100%!important}
-#ghostrun .ghost-run-shadow-fade{height:68px!important;bottom:-20px!important}
-#ghostrun .ghost-run-controls{padding-left:34px!important;padding-right:34px!important}
-`;
