74f0dec52ece716d0ade588aeaf1d16f6f122701
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-23 01:55:40 +0330

    Fix Ghost Run background layering

--- src/miniapp/ghost-run/styles.ts
+++ src/miniapp/ghost-run/styles.ts
@@ -485,29 +485,15 @@ body:has(#ghostrun.active) .tabs {
   animation-play-state: paused;
   will-change: background-position;
 }
-#ghostrun .ghost-run-uploaded-background-1 {
+#ghostrun .ghost-run-uploaded-background {
   background-image: url('/app/api/ghost-run-asset/background.png');
-  animation-name: ghostRunUploadedBackground1;
+  animation-name: ghostRunUploadedBackground;
   animation-duration: 18s;
-}
-#ghostrun .ghost-run-uploaded-background-2 {
-  background-image: url('/app/api/ghost-run-asset/background2.png');
-  animation-name: ghostRunUploadedBackground2;
-  animation-duration: 13s;
-  opacity: .82;
-}
-#ghostrun .ghost-run-uploaded-background-3 {
-  background-image: url('/app/api/ghost-run-asset/background3.png');
-  animation-name: ghostRunUploadedBackground3;
-  animation-duration: 8.5s;
-  opacity: .72;
 }
 #ghostrun .ghost-run-screen[data-ghost-state='running'] .ghost-run-uploaded-background {
   animation-play-state: running;
 }
-#ghostrun .ghost-run-screen[data-ghost-state='running'] .ghost-run-uploaded-background-1 { animation-duration: 10.5s; }
-#ghostrun .ghost-run-screen[data-ghost-state='running'] .ghost-run-uploaded-background-2 { animation-duration: 7.5s; }
-#ghostrun .ghost-run-screen[data-ghost-state='running'] .ghost-run-uploaded-background-3 { animation-duration: 5.2s; }
+#ghostrun .ghost-run-screen[data-ghost-state='running'] .ghost-run-uploaded-background { animation-duration: 10.5s; }
 #ghostrun .ghost-run-sky,
 #ghostrun .ghost-run-stars,
 #ghostrun .ghost-run-fog,
@@ -565,9 +551,7 @@ body:has(#ghostrun.active) .tabs {
 #ghostrun .ghost-run-screen[data-ghost-state='running'] .ghost-run-uploaded-house-2 { animation-duration: 4.6s; }
 #ghostrun .ghost-run-screen[data-ghost-state='running'] .ghost-run-uploaded-house-3 { animation-duration: 3.7s; }
 @keyframes ghostRunUploadedGround { to { background-position: -420px bottom; } }
-@keyframes ghostRunUploadedBackground1 { to { background-position: -900px center; } }
-@keyframes ghostRunUploadedBackground2 { to { background-position: -1100px center; } }
-@keyframes ghostRunUploadedBackground3 { to { background-position: -1300px center; } }
+@keyframes ghostRunUploadedBackground { to { background-position: -900px center; } }
 @keyframes ghostRunUploadedTree1 { to { background-position: -360px bottom; } }
 @keyframes ghostRunUploadedTree2 { to { background-position: -430px bottom; } }
 @keyframes ghostRunUploadedTree3 { to { background-position: -520px bottom; } }
