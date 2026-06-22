3373f40021da7e4eb545f4491edf95259f3d74ca
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-22 06:59:39 +0330

    Revert index style import

--- src/miniapp/ghost-run/index.ts
+++ src/miniapp/ghost-run/index.ts
@@ -1,5 +1,2 @@
 export { GHOST_RUN_SECTION } from './section';
-import { GHOST_RUN_STYLES as BASE_GHOST_RUN_STYLES } from './styles';
-import { GHOST_RUN_PAGE_FIT } from './page-fit';
-
-export const GHOST_RUN_STYLES = BASE_GHOST_RUN_STYLES + GHOST_RUN_PAGE_FIT;
+export { GHOST_RUN_STYLES } from './styles';
