1610c038758306d43f4b547aeccb25d638c8d087
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-25 23:40:01 +0330

    Revert "Make Ghost Run globally live (Durable Object + WebSocket)"

--- src/index-with-plinko-live.ts
+++ src/index-with-plinko-live.ts
@@ -5,5 +5,4 @@ import './withdrawal-admin-routes';
 
 export { SectionLockEvents } from './section-lock-events';
 export { PlinkoLiveRoom } from './plinko-live';
-export { GhostRunLiveRoom } from './ghost-run-live';
 export { default } from './index-with-fragment-detail-polish';
