390cd65a58bbbd551b425f5a010cd1933d98b423
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-08-10 09:18:35 +0330

    Expose one Crash style path

--- src/miniapp/crash/index.ts
+++ src/miniapp/crash/index.ts
@@ -1,4 +1,3 @@
 export { CRASH_SECTION } from './section';
 export { CRASH_STYLES } from './styles';
-export { CRASH_CONTROLS_MODERN_STYLES } from './control-styles';
 export { CRASH_SCRIPT } from './scripts/main';
